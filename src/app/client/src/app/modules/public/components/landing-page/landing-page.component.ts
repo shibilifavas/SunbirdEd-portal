05501fd02a1e24d56b91022652e99c94039ccaf4
shoaib-mohmad <shoaibmohammad@Netopss-MacBook-Pro.local>
2023-08-11 11:25:58 +0530

    Fixed issues.

--- src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
+++ src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { LayoutService } from '@sunbird/shared';
 import * as publicService from '../../services';
-import {SearchService} from './../../../core/services/search/search.service';
+import { SearchService } from './../../../core/services/search/search.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,14 +9,15 @@ import {SearchService} from './../../../core/services/search/search.service';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  configContent:any = {}
-  CAROUSEL_BREAKPOINT = 768;
+  configContent: any = {}
+  // CAROUSEL_BREAKPOINT = 768;
+  CAROUSEL_BREAKPOINT = 1400;
   carouselDisplayMode = 'multiple';
-  courses:any = {};
-  
+  courses: any = {};
+
   layoutConfiguration;
 
-  constructor(public layoutService: LayoutService, private landingPageContentService:publicService.LandingPageContentService, public search: SearchService) { }
+  constructor(public layoutService: LayoutService, private landingPageContentService: publicService.LandingPageContentService, public search: SearchService) { }
 
   ngOnInit() {
     // alert()
@@ -24,7 +25,81 @@ export class LandingPageComponent implements OnInit {
     this.landingPageContentService.getPageContent().subscribe(res => {
       this.configContent = res;
     })
-    this.search.compositeSearch({}).subscribe(res => {
+    // this.search.compositeSearch({}).subscribe(res => {
+    //   this.courses = res;
+    //   console.log('Courses', this.courses);
+    // })
+    let requestData = {
+      "request": {
+        "filters": {
+          "channel": "0138193046778920963",
+          "primaryCategory": [
+            "Collection",
+            "Resource",
+            "Content Playlist",
+            "Course",
+            "Course Assessment",
+            "Digital Textbook",
+            "eTextbook",
+            "Explanation Content",
+            "Learning Resource",
+            "Lesson Plan Unit",
+            "Practice Question Set",
+            "Teacher Resource",
+            "Textbook Unit",
+            "LessonPlan",
+            "FocusSpot",
+            "Learning Outcome Definition",
+            "Curiosity Questions",
+            "MarkingSchemeRubric",
+            "ExplanationResource",
+            "ExperientialResource",
+            "Practice Resource",
+            "TVLesson",
+            "Course Unit"
+          ],
+          "visibility": [
+            "Default",
+            "Parent"
+          ]
+        },
+        "limit": 100,
+        "sort_by": {
+          "lastPublishedOn": "desc"
+        },
+        "fields": [
+          "name",
+          "appIcon",
+          "mimeType",
+          "gradeLevel",
+          "identifier",
+          "medium",
+          "pkgVersion",
+          "board",
+          "subject",
+          "resourceType",
+          "primaryCategory",
+          "contentType",
+          "channel",
+          "organisation",
+          "trackable"
+        ],
+        "softConstraints": {
+          "badgeAssertions": 98,
+          "channel": 100
+        },
+        "mode": "soft",
+        "facets": [
+          "se_boards",
+          "se_gradeLevels",
+          "se_subjects",
+          "se_mediums",
+          "primaryCategory"
+        ],
+        "offset": 0
+      }
+    };
+    this.landingPageContentService.getCourses(requestData).subscribe(res => {
       this.courses = res;
       console.log('Courses', this.courses);
     })
