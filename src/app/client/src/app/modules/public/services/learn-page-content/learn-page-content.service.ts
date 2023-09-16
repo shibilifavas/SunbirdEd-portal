7158bd4725df69c848fbfd7e2e76384d62489caf
DeviGopinath <devigopinath111@gmail.com>
2023-09-16 14:20:08 +0530

    recently published courses UI in learn page

--- src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
+++ src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +19,20 @@ export class LearnPageContentService {
   getBrowseByCompetencyData(data: any){
     return this.http.post(`${this.competencyApiUrl}/search?orgdetails=orgName,email&licenseDetails=name,description,url`, data);
   }
+
+  search(requestBody) {
+    const url = `/api/content/v1/search?orgdetails=orgName,email`;
+    return this.http.post(url, requestBody)
+      .pipe(map((val: any) => {
+        if (val.result && val.result.content) {
+          val.result.content.forEach(element => this.updateCardData(element));
+        }
+        return val.result;
+      }));
+  }
+
+  updateCardData(content) {
+    content.cardImg =  content.appIcon || content.courseLogoUrl || content.cardImg || 'assets/images/book.png';
+    return content;
+   }
 }
