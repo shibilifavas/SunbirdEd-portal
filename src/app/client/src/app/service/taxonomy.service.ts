6deb685d5881ceb5f894dcb00dec5328bd1d01b9
shoaib-mohmad <shoaibmohammad@Netopss-MacBook-Pro.local>
2023-09-20 11:50:24 +0530

    WOrk in progress

--- src/app/client/src/app/service/taxonomy.service.ts
+++ src/app/client/src/app/service/taxonomy.service.ts
@@ -1,7 +1,6 @@
 // import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TaxonomyCategories } from '../framework.config';
-import { find } from 'lodash';
+import { TaxonomyCategories } from './../framework.config';
 // import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,18 +11,7 @@ export class TaxonomyService {
   constructor() { }
 
   getTaxonomyCategories(){
-    localStorage.setItem('taxonomyCategories', JSON.stringify(TaxonomyCategories));
     return TaxonomyCategories;
   }
 
-  getCategoryforHTML(obj: any, category: string) {
-    if(obj) {
-      let c = find(TaxonomyCategories, c => {return category == c});
-      return obj[c];      
-    }
-    return null;
-  }
-  capitalizeFirstLetter(str: string): string {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
 }
