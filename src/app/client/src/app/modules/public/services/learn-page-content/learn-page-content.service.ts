b1ba9ec1180b3cc6e272aef341b05cd836ad9a3f
DeviGopinath <devigopinath111@gmail.com>
2023-09-27 22:30:51 +0530

    dynamic global filters issue

--- src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
+++ src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
@@ -7,9 +7,6 @@ import { map } from 'rxjs/operators';
 })
 export class LearnPageContentService {
 
-  // competencyApiUrl = envHelper.CONTENT_URL;
-  competencyApiUrl = 'https://compass-dev.tarento.com/api/content/v1';
-
   constructor(private http: HttpClient) { }
 
   getPageContent(){
@@ -17,6 +14,6 @@ export class LearnPageContentService {
   }
 
   getBrowseByCompetencyData(data: any){
-    return this.http.post(`${this.competencyApiUrl}/search?orgdetails=orgName,email&licenseDetails=name,description,url`, data);
+    return this.http.post(`/api/content/v1/search?orgdetails=orgName,email&licenseDetails=name,description,url`, data);
   }
 }
