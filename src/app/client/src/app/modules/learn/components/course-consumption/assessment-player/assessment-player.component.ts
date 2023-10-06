f82aa04f7f3fb7aca85db51ef5eeac1ec3b751b8
pavankumar0408 <131791648+pavankumar0408@users.noreply.github.com>
2023-10-06 14:20:14 +0530

    Course progress (#116)
    * Rating disabled and your progress updated
    
    * questionset progress added
    
    * removed unwanted code
    
    * consumption library updated
    
    * score for total Max score updated
    
    * course progress update
    
    * pdf resume progress changes
    
    * Video and pdf resume changes
    
    * resume feature for video and pdf updated
    
    * totalcount updated

--- src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
@@ -377,6 +377,7 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
   private getContentState() {
     if (this.batchId && (_.get(this.activeContent, 'contentType') === 'SelfAssess' || !this.isRouterExtrasAvailable)) {
       const req: any = this.getContentStateRequest(this.courseHierarchy);
+      this.totalCount = req.contentIds?.length;
       this.CsCourseService
       .getContentState(req, { apiPath: '/content/course/v1' })
       .pipe(takeUntil(this.unsubscribe))
@@ -389,7 +390,6 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
           }
         });
         // this.pagesVisited = res.content[0].progressdetails?.current;
-        this.totalCount = res.totalCount
         const _contentIndex = _.findIndex(this.contentStatus, {contentId: _.get(this.activeContent, 'identifier')});
         const _resIndex =  _.findIndex(res.content, {contentId: _.get(this.activeContent, 'identifier')});
         if (_.get(this.activeContent, 'contentType') === 'SelfAssess' && this.isRouterExtrasAvailable) {
@@ -942,10 +942,11 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
             config.context.objectRollup = this.objectRollUp;
           }
           this.playerConfig = config;
-          if(this.visitedData?.length > 0 && this.playerConfig.metadata.mimeType == 'application/pdf') {
-            this.playerConfig.config.pagesVisited = this.visitedData;
-          } else if(this.visitedData?.length > 0 && this.playerConfig.metadata.mimeType == 'video/mp4') {
-            this.playerConfig.config.currentDuration = this.visitedData[0];
+          let consumedData: any = this.CourseProgressService.contentProgress['content_'+`${this.selectedContentId}`];
+          if((consumedData?.length > 0 || this.visitedData?.length > 0) && this.playerConfig.metadata.mimeType == 'application/pdf') {
+            this.playerConfig.config.pagesVisited = consumedData || this.visitedData;
+          } else if((consumedData?.length > 0 || this.visitedData?.length > 0) && this.playerConfig.metadata.mimeType == 'video/mp4') {
+            this.playerConfig.config['currentDuration'] = consumedData[0] || this.visitedData[0];
           }
           const _contentIndex = _.findIndex(this.contentStatus, { contentId: _.get(config, 'context.contentId') });
           this.playerConfig['metadata']['maxAttempt'] = _.get(this.activeContent, 'maxAttempts');
