16a511719fbcae3eb729ebd8229030213b684baf
santhosh-tarento <120555456+santhosh-tarento@users.noreply.github.com>
2023-10-27 10:31:02 -0400

    Update framework.config.ts

--- src/app/client/src/app/framework.config.ts
+++ src/app/client/src/app/framework.config.ts
@@ -14,13 +14,13 @@ export const WebExtensionsConfig = {
 
 export const PluginModules = [ OfflineModule ];
 
-export const TaxonomyCategories = ['taxonomyCategory1', 'taxonomyCategory2', 'taxonomyCategory3', 'taxonomyCategory4'];
+export const TaxonomyCategories = ['taxonomyCategory1', 'taxonomyCategory2', 'taxonomyCategory3', 'taxonomyCategory4', 'taxonomyCategory5'];
 
 export const taxonomyEnvironment = {
   production: false,
   url: '',
   token: '',
-  frameworkName: 'tarento_fw',
+  frameworkName: 'TLP_FW',
   channelId: 'tarento',
   authToken: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0WEFsdFpGMFFhc1JDYlFnVXB4b2RvU2tLRUZyWmdpdCJ9.mXD7cSvv3Le6o_32lJplDck2D0IIMHnv0uJKq98YVwk',
   userToken:'',
@@ -29,65 +29,7 @@ export const taxonomyEnvironment = {
 
 export const taxonomyConfig = [
   {
-      "frameworkId" :"compass_fw",
-      "config" : [
-          {   
-              "index": 1,
-              "category": "board",
-              "icon": "settings",
-              "color": "#1d2327"
-          },
-          {   
-              "index": 2,
-              "category": "medium",
-              "icon": "extension",
-              "color": "#541675"
-          },
-          {   
-              "index": 3,
-              "category": "gradeLevel",
-              "icon": "bar_chart",
-              "color": "#9a6c80"
-          },
-          {   
-              "index": 4,
-              "category": "subject",
-              "icon": "account_box",
-              "color": "#d8666a"
-          }
-      ]
-  },
-  {
-      "frameworkId" :"tarento_fw",
-      "config" : [
-          {   
-              "index": 1,
-              "category": "taxonomyCategory1",
-              "icon": "settings",
-              "color": "#1d2327"
-          },
-          {   
-              "index": 2,
-              "category": "taxonomyCategory2",
-              "icon": "extension",
-              "color": "#541675"
-          },
-          {   
-              "index": 3,
-              "category": "taxonomyCategory3",
-              "icon": "bar_chart",
-              "color": "#9a6c80"
-          },
-          {   
-              "index": 4,
-              "category": "taxonomyCategory4",
-              "icon": "account_box",
-              "color": "#d8666a"
-          }
-      ]
-  },
-  {
-    "frameworkId" :"gov_fw",
+    "frameworkId" :"TLP_FW",
     "config" : [
         {   
             "index": 1,
@@ -106,11 +48,23 @@ export const taxonomyConfig = [
             "category": "taxonomyCategory3",
             "icon": "bar_chart",
             "color": "#9a6c80"
+        },
+        {   
+            "index": 4,
+            "category": "taxonomyCategory4",
+            "icon": "account_box",
+            "color": "#d8666a"
+        },
+        {   
+            "index": 5,
+            "category": "taxonomyCategory5",
+            "icon": "bar_chart",
+            "color": "#ed8699"
         }
     ]
   },
   {
-    "frameworkId" :"fracing_fw",
+    "frameworkId" :"dpg_fw",
     "config" : [
         {   
             "index": 1,
@@ -144,5 +98,4 @@ export const taxonomyConfig = [
         }
     ]
   }
-
 ]
