95de4f3f406ec8fe09c870f8a1a249bf5b7a2083
deviigopinath <138746944+deviigopinath@users.noreply.github.com>
2023-11-20 11:53:11 +0530

    UI fixes (#206)
    * competency search page title issue fixed
    
    * responsiveness fixed for toc
    
    * fields modified for edit profile
    
    * fields modified for edit profile
    
    * update api issue fixed
    
    ---------
    
    Co-authored-by: DeviGopinath <devigopinath111@gmail.com>

--- src/app/client/src/app/plugins/profile/components/personal-details/personal-details.component.ts
+++ src/app/client/src/app/plugins/profile/components/personal-details/personal-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {UserService} from '@sunbird/core';
-import {IUserData} from '@sunbird/shared';
+import {IUserData, ToasterService, ResourceService} from '@sunbird/shared';
 import { ProfileService } from '@sunbird/profile';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-personal-details',
@@ -11,11 +12,11 @@ import { ProfileService } from '@sunbird/profile';
 })
 export class PersonalDetailsComponent implements OnInit {
   form: FormGroup;
-  formData = {"colOne":{"fields":[{"label":"First name","value":"firstName"},{"label":"Surname","value":"surname"},{"label":"Mobile number","value1":"code","value2":"mobile"},{"label":"Primary email","value":"primaryEmail"},{"label":"Secondary email","value":"secondaryEmail"}],"radio":[]},"colTwo":{}}
+  formData = {"colOne":{"fields":[{"label":"First name","value":"firstName"},{"label":"Last name","value":"lastName"},{"label":"Mobile number","value1":"code","value2":"mobile"},{"label":"Primary email","value":"primaryEmail"},{"label":"Secondary email","value":"secondaryEmail"},{"label":"Department name","value":"departmentName"},{"label":"Designation","value":"designation"},{"label":"Date of joining","value":"doj"}],"radio":[]},"colTwo":{}}
   userProfile: any;
   payload: any = {};
 
-  constructor(private formBuilder: FormBuilder, public userService: UserService, private profileService: ProfileService,) { }
+  constructor(private formBuilder: FormBuilder, public userService: UserService, private profileService: ProfileService, public toasterService: ToasterService, public resourceService: ResourceService) { }
 
   ngOnInit(): void {
     this.userService.userData$.subscribe((user: IUserData) => {
@@ -26,10 +27,13 @@ export class PersonalDetailsComponent implements OnInit {
     console.log("user profile", this.userProfile);
     this.form = this.formBuilder.group({
       firstName: [this.userProfile.firstName, Validators.required],
-      surname: [this.userProfile.lastName, Validators.required],
-      mobile: [this.userProfile.phone, Validators.required],
-      primaryEmail: [{value: this.userProfile.email, disabled: true}, Validators.required],
-      secondaryEmail: ['', Validators.required]
+      lastName: [this.userProfile.lastName, Validators.required],
+      mobile: [this.userProfile.phone],
+      primaryEmail: [{value: this.userProfile.email, disabled: true}],
+      secondaryEmail: [''],
+      departmentName: ['', Validators.required],
+      designation: ['', Validators.required],
+      doj: ['', Validators.required]
     });
   }
 
@@ -38,8 +42,27 @@ export class PersonalDetailsComponent implements OnInit {
       console.log('Form submitted!', this.form.value);
       this.payload = this.form.value;
       this.payload.userId = this.userProfile.userId;
+      let personalDetails: any = {};
+      personalDetails.firstName = this.payload.firstName;
+      personalDetails.primaryEmail = this.payload.primaryEmail;
+      this.payload.personalDetails = personalDetails;
+      let profileDetails: any = {};
+      let employmentDetails: any = {};
+      employmentDetails.departmentName = this.payload.departmentName;
+      delete this.payload.departmentName;
+      profileDetails.employmentDetails = employmentDetails;
+      this.payload.profileDetails = profileDetails;
+      let professionalDetails: any = [];
+      let professionalDetail: any = {};
+      professionalDetail.designation = this.payload.designation;
+      professionalDetail.doj = this.payload.doj;
+      professionalDetails.push(professionalDetail);
+      delete this.payload.designation;
+      delete this.payload.doj;
+      this.payload.professionalDetails = professionalDetails;
       this.profileService.updatePrivateProfile(this.payload).subscribe(res => {
         console.log("res",res);
+        this.toasterService.success(_.get(this.resourceService, 'messages.smsg.m0059'));
     })
     }
   }
