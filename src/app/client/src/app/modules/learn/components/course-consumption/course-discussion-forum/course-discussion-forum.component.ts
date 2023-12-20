f35de669304f8f886d9da0638958e9f87219c125
Shoaib Mohmad <shoaib.mohmad@tarento.com>
2023-12-20 13:40:54 +0530

    Df latest (#252)
    * Added the changes for DF.
    
    * Updated the discussion forum in mega menu.
    
    * Integrated API to get username & identifier in general discussions.
    
    * Created DF for course.
    
    ---------
    
    Co-authored-by: shoaib_mohmad <shoaibmohammad@Netopss-MacBook-Pro.local>

--- src/app/client/src/app/modules/learn/components/course-consumption/course-discussion-forum/course-discussion-forum.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/course-discussion-forum/course-discussion-forum.component.ts
@@ -1,4 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IForumContext } from '../../../interfaces/course';
+import { CoursesService, PermissionService, CopyContentService,
+  OrgDetailsService, UserService, GeneraliseLabelService,  } from '@sunbird/core';
+import _ from 'lodash';
+import { combineLatest as observableCombineLatest, Subject } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DiscussionService } from './../../../../discussion/services/discussion/discussion.service';
+import { ToasterService, ResourceService } from '@sunbird/shared';
 
 @Component({
   selector: 'course-discussion-forum',
@@ -8,16 +16,83 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CourseDiscussionForumComponent implements OnInit {
   @Input() courseDetails: any;
   @Input() configContent: any;
+  @Input() courseHierarchy: any;
   discussionText:string = '';
   postText:string = '';
   searchText:string = '';
   fields = ['Popular', 'Recently posted'];
   selectedField = 'Recently posted';
   writeComment = false;
+    /**
+   * input data for fetchforum Ids
+   */
+     fetchForumIdReq: any;
+     courseId: string;
+     enrolledCourse = false;
+     batchId: any;
 
-  constructor() { }
+  constructor(public router: Router, private activatedRoute: ActivatedRoute, 
+    private userService: UserService, public permissionService: PermissionService, 
+    private discussionService: DiscussionService, private toasterService: ToasterService,
+    private resourceService: ResourceService) { }
 
   ngOnInit(): void {
+    this.generateDataForDF();
+  }
+
+  generateDataForDF() {
+    // alert(1);
+    const isCreator = this.userService.userid === _.get(this.courseDetails, 'createdBy');
+    const isMentor = this.permissionService.checkRolesPermissions(['COURSE_MENTOR']);
+
+    observableCombineLatest(this.activatedRoute.firstChild.params, this.activatedRoute.firstChild.queryParams,
+      (params, queryParams) => {
+        return { ...params, ...queryParams };
+      }).subscribe((params) => {
+        this.courseId = params.courseId;
+        this.batchId = params.batchId;
+      });
+
+    if (isCreator) {
+      this.fetchForumIdReq = {
+        type: 'course',
+        identifier: [this.courseId],
+        cid: 6
+      };
+    } else if (this.enrolledCourse) {
+      this.fetchForumIdReq = {
+        type: 'batch',
+        identifier: [this.batchId],
+        cid: 6
+      };
+    } else if (isMentor) {
+      // TODO: make getBatches() api call;
+      this.fetchForumIdReq = {
+        type: 'course',
+        identifier: [this.courseId],
+        cid: 6
+      };
+    }
+
+    this.discussionService.createForum(this.fetchForumIdReq).subscribe(resp => {
+      console.log('Create forum:', resp);
+    }, error => {
+      this.toasterService.error(this.resourceService.messages.emsg.m0005);
+    });
+  }
+
+  /**
+     * @description - navigate to the DF Page when the event is emited from the access-discussion component
+     * @param  {} routerData
+     */
+   assignForumData(routerData:any) {
+    //  alert(routerData.usenavigateToDiscussionForumrId);
+    this.router.navigate(['/discussion-forum'], {
+      queryParams: {
+        categories: JSON.stringify({ result: routerData.forumIds }),
+        userId: routerData.userId
+      }
+    });
   }
 
 }
