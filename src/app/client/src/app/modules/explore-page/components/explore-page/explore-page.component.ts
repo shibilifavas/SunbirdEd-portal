98336923cda6fbecbbf727ddd90319654a8a0dd7
santhosh-tarento <120555456+santhosh-tarento@users.noreply.github.com>
2024-01-15 03:08:35 -0500

    fields (#313)

--- src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
+++ src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
@@ -368,7 +368,7 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
             "icon": "school",
             "link": ""
         });
-        this.checkUserProfileDetails();
+        // this.checkUserProfileDetails();
     }
 
     public getWishlisteddoIds() {
