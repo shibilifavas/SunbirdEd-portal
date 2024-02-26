d8e752c6171d80244f830a9f663d31e776ea8c20
deviigopinath <138746944+deviigopinath@users.noreply.github.com>
2024-02-26 13:42:14 +0530

    Navigation (#361)
    * navigation to landing page issue
    
    * navigation issue fixed
    
    ---------
    
    Co-authored-by: DeviGopinath <devigopinath111@gmail.com>

--- src/app/client/src/app/modules/public/services/landingpage-guard/landingpage.guard.ts
+++ src/app/client/src/app/modules/public/services/landingpage-guard/landingpage.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from '@sunbird/environment';
 @Injectable()
 export class LandingpageGuard implements CanActivate {
@@ -8,16 +8,21 @@ export class LandingpageGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if (environment.isDesktopApp) {
-        this.router.navigate(['/mydownloads'], { queryParams: { selectedTab: 'mydownloads' }});
-        return false;
+      if(localStorage.getItem('userProfile')){   //checking if logged in
+          window.location.href = '/resources';
+          return of(true);
       } else {
-        const defaultTenantIndexStatus: string = (<HTMLInputElement>document.getElementById('defaultTenantIndexStatus')) ?
-        (<HTMLInputElement>document.getElementById('defaultTenantIndexStatus')).value : 'false';
-        if (defaultTenantIndexStatus.toLowerCase() === 'true') {
-          window.location.replace(window.location.origin);
+        if (environment.isDesktopApp) {
+          this.router.navigate(['/mydownloads'], { queryParams: { selectedTab: 'mydownloads' }});
+          return false;
+        } else {
+          const defaultTenantIndexStatus: string = (<HTMLInputElement>document.getElementById('defaultTenantIndexStatus')) ?
+          (<HTMLInputElement>document.getElementById('defaultTenantIndexStatus')).value : 'false';
+          if (defaultTenantIndexStatus.toLowerCase() === 'true') {
+            window.location.replace(window.location.origin);
+          }
+          return (defaultTenantIndexStatus.toLowerCase() !== 'true');
         }
-        return (defaultTenantIndexStatus.toLowerCase() !== 'true');
       }
   }
 }
